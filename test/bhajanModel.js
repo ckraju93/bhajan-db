d636fd076e8341ab6ca2fb403dcd111aab434b5e
Saish Gunturi <sai.gunturi@gmail.com>
2013-10-06 00:38:22 -0500

    Add tests file for Bhajan model.

---
+++ test/bhajanModel.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+var moment = require('moment');
+
+var Bhajan = require('../models/Bhajan.js');
+
+describe('Bhajan create', function () {
+});
+
+describe('Bhajan update', function () {
+});
+
+describe('Bhajan findOne', function () {});
+describe('Bhajan search', function () {});
