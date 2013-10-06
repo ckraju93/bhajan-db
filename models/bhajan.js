4a254c40c3faf81e6516e6c2fc55caa66ad0c63d
Saish Gunturi <sai.gunturi@gmail.com>
2013-10-06 00:32:41 -0500

    Start basic model properties.

---
+++ models/bhajan.js
@@ -0,0 +1,36 @@
+var moment = require('moment');
+var mongodb = require('mongodb');
+var _ = require('underscore');
+
+var mongoServer = 'localhost';
+var environment = 'development';
+
+var serverConnect = function (collection_name, callback) {
+    if (!_.isFunction(callback)) {
+        return callback(new Error('callback is not a function.'));
+    } else {
+        var server = new mongodb.Server(mongoServer, 27017, {auto_reconnect: true, safe: true});
+        new mongodb.Db(environment, server, {safe: true}).open(function (error, client) {
+            if (error) throw error;
+
+            var collection = new mongodb.Collection(client, collection_name);
+            callback(client, collection);
+        });
+    }
+};
+
+module.exports = {
+    create: function (data, callback) {
+
+    },
+    update: function (data) {
+
+    },
+    findOne: function (data) {
+
+    },
+    search: function (data) {
+
+    },
+    serverConnect: serverConnect
+}
