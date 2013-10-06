3766fbbfa85e41dd06a741d114e02bc6dcdfa5e2
Saish Gunturi <sai.gunturi@gmail.com>
2013-10-06 02:15:32 -0500

    Add test to check if mongod is running locally.

--- test/database.js
+++ test/database.js
@@ -1,8 +1,15 @@
 var expect = require('chai').expect;
 var request = require('superagent');
 
-describe('Database', function () {
-    it('should be running.', function () {
+var db_location = 'localhost:28017';
 
+describe('Database', function () {
+    it('should be running.', function (done) {
+        request.get(db_location).end(function (res) {
+            expect(res).to.exist;
+            expect(res.status).to.equal(200);
+            expect(res.text).to.contain('mongod');
+            done();
+        });
     });
 });
